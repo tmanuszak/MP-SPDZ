3237ffdc17ee84fb293419fae89ac67a44b86ee7
Trey Manuszak <tmanusza@asu.edu>
2022-03-17 13:39:17 -0700

    Added simulategaap.sh script

---
+++ simulategaap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# This file will run a GAAP protocol on various MPC protocols and output statistics about each.
+# This script takes the follwing arguments:
+#   bits_of_security: This is the desired bits of security for the protocol
+#   participants: This is the number of participants participating in the GAAP
+#   threshold: At least threshold number of honest participant secret shares are required for a successful authentication
+
