1fd53265d999b0faf422794631fa3af3a648acc9
Trey Manuszak <tmanusza@asu.edu>
2022-03-21 13:44:34 -0700

    generateshares.py works

--- generateshares.py
+++ generateshares.py
@@ -2,12 +2,14 @@
 # Arguments to this script are [participants] [threshold] [bitsofsecurity]
 
 import os
+import sys
 import random
 import functools
 from sympy import nextprime
 
-_PRIME = sympy.nextprime(sys.argv[3])  # We will be doing arithmetic in the GF(_PRIME) finite field
+_PRIME = nextprime(int(sys.argv[3]))  # We will be doing arithmetic in the GF(_PRIME) finite field
 _RINT = functools.partial(random.SystemRandom().randint, 0)
+_RINTNONZERO = functools.partial(random.SystemRandom().randint, 1)
 
 def inverse(a, prime=_PRIME):
     return pow(a, -1, prime)
@@ -21,7 +23,7 @@ def exponentiate(base, power, prime=_PRIME):
     elif power == 1:
         return base
     else:
-        return exponentiate((base * base) % prime), power - 1)
+        return (base * exponentiate(base , power - 1)) % prime
 
 def eval_at(poly, x, prime=_PRIME):
     accum = 0
@@ -34,15 +36,17 @@ def eval_at(poly, x, prime=_PRIME):
 def make_random_shares(threshold, shares, prime=_PRIME):
     if threshold > shares:
         raise ValueError("Threshold cannot be more than the number of shares.")
-    poly = [1] + [_RINT(prime - 1) for i in range(threshold - 1)]  # secret is always 1
+    poly = [1] + [_RINT(prime - 1) for i in range(threshold - 2)] + [_RINTNONZERO(prime - 1)]  # secret is always 1 and poly[threshold - 1] must be non-zero
     for i in range(shares):
-        os.system("echo " + eval_at(poly, i + 1, prime) + " > Player-Data/Input-P" + i + "-0")
+        os.system("echo " + str(eval_at(poly, i + 1, prime)) + " > Player-Data/Input-P" + str(i) + "-0")
     return
 
 def main():
-    numberofparticipants = sys.argv[1]
-    threshold = sys.argv[2]
-    bitsofsecurity = sys.argv[3]    
+    numberofparticipants = int(sys.argv[1])
+    threshold = int(sys.argv[2])
+    bitsofsecurity = int(sys.argv[3])
+    make_random_shares(threshold, numberofparticipants)
+    return
 
 if __name__ == "__main__":
     main()
